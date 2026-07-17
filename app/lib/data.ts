e78f4808163dc640f2e25600439e37ef0764ec78
stevenmondragon23 <smondragonflores@byupathway.edu>
2026-07-16 23:53:23 -0500

    chapter 10 complete

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,17 +11,26 @@ import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
+
+
+
+
+
+
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -32,6 +41,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -84,6 +95,10 @@ export async function fetchCardData() {
     throw new Error('Failed to fetch card data.');
   }
 }
+
+
+
+
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
